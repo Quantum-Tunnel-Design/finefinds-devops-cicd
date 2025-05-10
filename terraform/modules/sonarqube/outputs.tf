8786f7303a70fe6824ef2fb2078d95cd422df5e4
Amal Gamage <amalcg989@gmail.com>
2025-05-11 01:44:09 +0400

    fix: updated configs

--- terraform/modules/sonarqube/outputs.tf
+++ terraform/modules/sonarqube/outputs.tf
@@ -1,16 +1,19 @@
-output "sonarqube_password" {
-  description = "SonarQube database password"
-  value       = random_password.sonarqube_password.result
-  sensitive   = true
+output "service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.sonarqube.name
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.sonarqube.arn
+}
+
+output "security_group_id" {
+  description = "Security group ID of the SonarQube ECS tasks"
+  value       = aws_security_group.sonarqube.id
 }
 
 output "sonarqube_url" {
   description = "URL of the SonarQube instance"
-  value       = "http://${aws_lb.sonarqube.dns_name}:9000"
-}
-
-output "sonarqube_initial_password" {
-  description = "Initial admin password for SonarQube"
-  value       = "admin"  # Default SonarQube password
-  sensitive   = true
+  value       = "http://${var.alb_dns_name}:9000"
 } 
