b3f99ba8f639778f7c00ecb311df51292e1f5cda
Amal Gamage <amalcg989@gmail.com>
2025-05-12 00:57:20 +0400

    fix: updated

--- terraform/modules/monitoring/outputs.tf
+++ terraform/modules/monitoring/outputs.tf
@@ -1,7 +1,7 @@
-output "log_group_name" {
-  description = "Name of the CloudWatch log group"
-  value       = aws_cloudwatch_log_group.app.name
-}
+# output "log_group_name" {
+#   description = "Name of the CloudWatch log group"
+#   value       = aws_cloudwatch_log_group.app.name
+# }
 
 output "dashboard_arn" {
   description = "ARN of the CloudWatch dashboard"
