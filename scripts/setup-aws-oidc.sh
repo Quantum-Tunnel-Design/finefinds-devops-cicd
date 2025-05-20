6af7dda66e7286196db0554f48d715e16cad7df2
Amal Gamage <amal.gamage@mycomera.com>
2025-05-20 15:11:28 +0400

    fix: updated

--- scripts/setup-aws-oidc.sh
+++ scripts/setup-aws-oidc.sh
@@ -220,7 +220,7 @@ create_role_policy() {
                 "arn:aws:ecs:${AWS_REGION}:${ACCOUNT_ID}:service/finefinds-${env}-cluster/*",
                 "arn:aws:ecs:${AWS_REGION}:${ACCOUNT_ID}:service/FineFinds${env}EcsService*",
                 "arn:aws:ecs:${AWS_REGION}:${ACCOUNT_ID}:task-definition/*",
-                "arn:aws:iam::${ACCOUNT_ID}:role/github-actions-*",
+                "arn:aws:iam::${ACCOUNT_ID}:role/github-actions-*"
             ]
         },
         {
